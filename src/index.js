af1346f5118e70d33e6fa3ec155a3458d4e52e52
bagrounds <bagrounds@gmail.com>
2017-03-10 21:24:03 -0800

    [MINOR][ADD] curryOptions function and guards

--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@
 ;(function () {
   'use strict'
 
+  /* imports */
+  var guarded = require('guarded')
+  var funAssert = require('fun-assert')
+  var R = require('ramda')
+
+  var isFunction = funAssert.type('Function')
+  var isNumber = funAssert.type('Number')
+  var isArray = funAssert.type('Array')
+  var isStringArray = funAssert.type('[String]')
+  var isObject = funAssert.type('Object')
+
   /* exports */
-  module.exports = funCurry
+  module.exports = guarded({
+    inputs: [isFunction, isNumber, isArray],
+    f: funCurry,
+    output: isFunction
+  })
+
+  module.exports.options = guarded({
+    inputs: [isFunction, isStringArray, isObject],
+    f: curryOptions,
+    output: isFunction
+  })
 
   /**
    *
-   * @function module:funCurry.funCurry
+   * @function module:fun-curry.funCurry
    *
    * @param {Function} f - to curry
    * @param {Number} [arity] - number of arguments f should accept
@@ -26,8 +47,32 @@
       var newArgs = args.concat(Array.prototype.slice.call(arguments))
 
       return newArgs.length === arity
-        ? f.apply(null, newArgs)
+        ? R.apply(f, newArgs)
         : funCurry(f, arity, newArgs)
+    }
+  }
+
+  /**
+   *
+   * @function module:fun-curry.options
+   *
+   * @param {Function} f - to curry
+   * @param {Array<String>} keys - all keys required prior to executing f
+   * @param {Object} [options] - initial options to apply
+   *
+   * @return {Function} { k1 } -> { k2 } -> ... -> f({ k1, k2, ... })
+   */
+  function curryOptions (f, keys, options) {
+    options = options || {}
+
+    return function optionsCurried (partialOptions) {
+      var newOptions = R.merge(options, partialOptions)
+
+      console.log(newOptions)
+
+      return R.difference(keys, R.keys(newOptions)).length === 0
+        ? R.call(f, newOptions)
+        : curryOptions(f, keys, newOptions)
     }
   }
 })()
